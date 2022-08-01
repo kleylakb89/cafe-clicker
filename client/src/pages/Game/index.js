7d6c211cf5a884cc30f1ceda867d3873e2fccd25
Ben Ashworth <bashworthj@gmail.com>
2022-08-01 18:17:15 -0400

    added elements and styling to home page and began the media queries

--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -11,9 +11,29 @@ function Game() {
         <div className="layout">
           <div className="container-fluid"></div>
           <h1 className="cafe-title">Cafe Clicker</h1>
-          <div className="my-auto">
+          <div className="toast">
             <img src={Avac1} alt="Avacado" width="700px" className="" />
           </div>
+          <div className="counter">Click Count:</div>
+          <div className="num-of-clicks">043</div>
+          <div className="power-title">Power Ups</div>
+          <div className="powerups">
+            <button className="power-btn">Butter Me Up</button>
+            <button className="power-btn">Add Garlic </button>
+            <button className="power-btn">Holy Shit Thats Good</button>
+          </div>
+          <div className="game-funcs">
+            <button className="game-btn">Save Game</button>
+            <button className="game-btn">Submit Score</button>
+          </div>
+         
+            <h2 className="how-title">Instructions:</h2>
+            <div className="instructions">
+            <p className="how-play">Click the avocado toast as many times as your fingers will allow! As the week goes by, and the clicks add up, you will receive power ups to help you gain more clicks.
+            To begin, simply click the avocado toast and keep on clicking</p>
+
+            </div>
+         
         </div>
       </div>
     </div>
