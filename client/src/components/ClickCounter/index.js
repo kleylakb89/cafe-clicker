07548df8a07de5ee6ce8fdd409f1dbd0df597d05
Kristin Kleyla <clever_disguises@yahoo.com>
2022-08-02 12:27:02 -0400

    worked on fixing click count

--- client/src/components/ClickCounter/index.js
+++ client/src/components/ClickCounter/index.js
@@ -1,19 +1,15 @@
 import React, { useState, } from 'react';
 import Avac1 from "../../images/Avac1.png";
 
-export default function ClickCounter() {
-  let [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount((count + 1));
-  };
+export default function ClickCounter({count, handleClick}) {
 
   return (
     <>
-    <div className="toast">
-    <button type="button"><img src={Avac1} alt="Avocado" width="700px" className="avac-img" onClick={handleClick}/></button></div>
-          <div className="counter">Click Count:</div>
-    <div className="num-of-clicks">{count}</div>
+      <div className="toast">
+        <div><img src={Avac1} alt="Avocado" width="700px" className="avac-img" onClick={handleClick} /></div>
+      </div>
+      <div className="counter">Click Count:</div>
+      <div className="num-of-clicks">{count}</div>
     </>
   );
 }
