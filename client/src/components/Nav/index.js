68b30f3ea0bfad3bb8827ed3f7696e52408235a6
Kristin Kleyla <clever_disguises@yahoo.com>
2022-08-04 11:46:03 -0400

    worked a bit on nav bar media queries

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -13,11 +13,11 @@ function Nav() {
     <div className="navbar-container">
         <ul className="menu-items">
             <li><a href="/game">Home</a></li>
-            <li><a  href="/" onClick={signout}>Logout</a></li>
+            <li><a href="/" onClick={signout}>Logout</a></li>
             <li><a href="/leaderboard">Leader Board</a></li>
             <li><a href="#view-cafe">View Cafe</a></li>
         </ul>
-        <img src={spatula }className="logo" width="100" ></img>
+        <img src={spatula} alt="spatula" className="logo" width="100" ></img>
     </div>
 </nav>
     
